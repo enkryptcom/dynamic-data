6e556f38fb2b0cc155598b64e784db9cf62c822e
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2023-03-18 16:42:38 -0700

    devop: move native token info here

--- swap-tokens-generator/src/list-handlers/changelly/index.ts
+++ swap-tokens-generator/src/list-handlers/changelly/index.ts
@@ -1,35 +1,13 @@
 import { NATIVE_ADDRESS } from "@src/configs";
 import { NetworkName, Token } from "@src/types";
 import fetch from "node-fetch";
+import {
+  ChangellyContractMap,
+  ChangellyPlatforms,
+  NativeTokens,
+} from "./utils";
 
 const CHANGELLY_BASE = `https://swap.mewapi.io/changelly`;
-
-const ChangellyPlatforms: {
-  [key in NetworkName]?: string;
-} = {
-  [NetworkName.Ethereum]: "ethereum",
-  [NetworkName.Matic]: "polygon",
-  [NetworkName.Binance]: "binance_smart_chain",
-  [NetworkName.EthereumClassic]: "ethereum_classic",
-  [NetworkName.Avalanche]: "avaxc",
-  [NetworkName.Klaytn]: "klaytn",
-  [NetworkName.Optimism]: "optimism",
-  [NetworkName.Moonbeam]: "glmr",
-};
-const ChangellyContractMap: {
-  [key in NetworkName]?: Record<string, string>;
-} = {
-  [NetworkName.Avalanche]: {
-    gmx: "0x62edc0692bd897d2295872a9ffcac5425011c661",
-    joe: "0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd",
-    usdtavac: "0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7",
-    usdcavac: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e",
-    qi: "0x8729438eb15e2c8b576fcc6aecda6a148776c0f5",
-  },
-  [NetworkName.Moonbeam]: {
-    glmr: NATIVE_ADDRESS,
-  },
-};
 
 interface ChangellyCurrency {
   name: string;
@@ -100,6 +78,9 @@ export default async (): Promise<ChangellyCurrency[]> =>
       const filtered = json.result.filter(
         (cur) => cur.enabled && cur.enabledFrom && cur.enabledTo
       );
+      filtered.forEach((item) => {
+        if (NativeTokens[item.ticker]) item.token = NativeTokens[item.ticker];
+      });
       filtered.sort((a, b) => a.fullName.localeCompare(b.fullName));
       return filtered;
     });
---
+++ swap-tokens-generator/src/list-handlers/changelly/utils.ts
@@ -0,0 +1,60 @@
+import { NATIVE_ADDRESS } from "@src/configs";
+import { NetworkName, Token } from "@src/types";
+
+const ChangellyPlatforms: {
+  [key in NetworkName]?: string;
+} = {
+  [NetworkName.Ethereum]: "ethereum",
+  [NetworkName.Matic]: "polygon",
+  [NetworkName.Binance]: "binance_smart_chain",
+  [NetworkName.EthereumClassic]: "ethereum_classic",
+  [NetworkName.Avalanche]: "avaxc",
+  [NetworkName.Klaytn]: "klaytn",
+  [NetworkName.Optimism]: "optimism",
+  [NetworkName.Moonbeam]: "glmr",
+};
+const ChangellyContractMap: {
+  [key in NetworkName]?: Record<string, string>;
+} = {
+  [NetworkName.Avalanche]: {
+    gmx: "0x62edc0692bd897d2295872a9ffcac5425011c661",
+    joe: "0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd",
+    usdtavac: "0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7",
+    usdcavac: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e",
+    qi: "0x8729438eb15e2c8b576fcc6aecda6a148776c0f5",
+  },
+  [NetworkName.Moonbeam]: {
+    glmr: NATIVE_ADDRESS,
+  },
+};
+
+const NativeTokens: Record<string, Token> = {
+  dot: {
+    address: NATIVE_ADDRESS,
+    decimals: 10,
+    logoURI:
+      "https://assets.coingecko.com/coins/images/12171/thumb/polkadot.png",
+    name: "Polkadot",
+    symbol: "DOT",
+    cgId: "polkadot",
+  },
+  ksm: {
+    address: NATIVE_ADDRESS,
+    decimals: 12,
+    logoURI:
+      "https://assets.coingecko.com/coins/images/9568/thumb/m4zRhP5e_400x400.jpg",
+    name: "Kusama",
+    symbol: "ksm",
+    cgId: "kusama",
+  },
+  btc: {
+    address: NATIVE_ADDRESS,
+    decimals: 8,
+    logoURI: "https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png",
+    name: "Bitcoin",
+    symbol: "BTC",
+    cgId: "bitcoin",
+  },
+};
+
+export { ChangellyContractMap, ChangellyPlatforms, NativeTokens };
